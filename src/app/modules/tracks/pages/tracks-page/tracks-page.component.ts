4a625d708f3396f76312850fdb724360e9f69204
Emir Moretti <emir.moretti@hotmail.com.ar>
2022-11-20 16:39:46 -0300

    tracksmodels json implement

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import * as dataRaw from '../../../../data/tracks.json';
 
 @Component({
   selector: 'app-tracks-page',
@@ -7,18 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TracksPageComponent implements OnInit {
 
-  mockTracksList = [
-    {
-      name: 'Bebe oficial',
-    },
-    {
-      name: 'pepe'
-    }
-  ]
+  mockTracksList: Array<TrackModel> = []
 
   constructor() { }
 
   ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default;
+    this.mockTracksList = data
   }
 
 }
